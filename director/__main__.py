e7cf77fd718f50bf79fc3de17086a0d91a02089c
SahilDave04 <sahildave04@gmail.com>
2026-07-17 22:52:29 +0530

    Add project files.

---
+++ director/__main__.py
@@ -0,0 +1,9 @@
+# Importing Necessary Modules
+from director.tree_cli import directoryTreeCli
+
+# Main entry point for the CLI
+if __name__ == '__main__':
+
+    # Invoking Directory Tree CLI Function
+    directoryTreeCli()
+
