77dd89ec8a78773f4009bb2f5d558d9ebcf122cd
SahilDave04 <sahildave04@gmail.com>
2026-07-18 12:32:48 +0530

    Testing Time !!

--- tests/V1.0.0_test.py
+++ tests/V1.0.0_test.py
@@ -2,7 +2,7 @@ from director import DisplayTree
 
 if __name__ == "__main__":
        
-    custom_path = "custom_path"
+    custom_path = ".//tests//Main_Directory"
 
     # 1. Test for String Representation
     stringRepresentation = DisplayTree(custom_path,stringRep=True)
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     print(stringRepresentation)
 
     # 3. Test for Console-Print and Max Depth >> Actual Depth
-    DisplayTree(custom_path,maxDepth=2,ignoreList=["ignore1"])
+    DisplayTree(custom_path,maxDepth=2,ignoreList=["__pycache__"])
 
     # 4. Test for Console-Print, Header for [OS, Path] Info and Not Show Hidden Files/Directories
     DisplayTree(header=True, maxDepth=100, showHidden=False)
@@ -22,9 +22,9 @@ if __name__ == "__main__":
     DisplayTree(maxDepth=2, showHidden=True)
 
     # 6. Test for Ignore Files / Directories [Absolute Names]
-    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["ignore1"])
+    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["__pycache__"])
 
     # 7. Test for Export TXT
-    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["ignore1"], exportTxt=True)
+    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["__pycache__"], exportTxt=True)
 
    
