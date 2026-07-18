53023ab4c286a51804fbe58016acef54ff70be15
SahilDave04 <sahildave04@gmail.com>
2026-07-18 12:24:07 +0530

    Testing Time !!

--- tests/V1.0.0_test.py
+++ tests/V1.0.0_test.py
@@ -1,17 +1,19 @@
 from director import DisplayTree
 
 if __name__ == "__main__":
+       
+    custom_path = "custom_path"
 
     # 1. Test for String Representation
-    stringRepresentation = DisplayTree(stringRep=True)
-    print(stringRepresentation)
+    stringRepresentation = DisplayTree(custom_path,stringRep=True)
+    #print(stringRepresentation)
 
     # 2. Test for Header in String Representation
-    stringRepresentation = DisplayTree(stringRep=True, header=True)
+    stringRepresentation = DisplayTree(custom_path,stringRep=True, header=True)
     print(stringRepresentation)
 
     # 3. Test for Console-Print and Max Depth >> Actual Depth
-    DisplayTree(maxDepth=1)
+    DisplayTree(custom_path,maxDepth=2,ignoreList=["ignore1"])
 
     # 4. Test for Console-Print, Header for [OS, Path] Info and Not Show Hidden Files/Directories
     DisplayTree(header=True, maxDepth=100, showHidden=False)
@@ -20,7 +22,9 @@ if __name__ == "__main__":
     DisplayTree(maxDepth=2, showHidden=True)
 
     # 6. Test for Ignore Files / Directories [Absolute Names]
-    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["*.py"])
+    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["ignore1"])
 
     # 7. Test for Export TXT
-    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["*.py"], exportTxt=True)
+    DisplayTree(maxDepth=4, showHidden=True, ignoreList=["ignore1"], exportTxt=True)
+
+   
